01af1c4ea24341a8373a11bd8c161317df07d5a7
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-01-28 19:54:58 -0700

    Generate absolute URLs for iframes on startup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,17 @@ do
     echo "Couldn't reach MQTT. Will retry in 5 seconds."
     sleep 5
 done
+
+mkdir /run/frames
+if cat /run/secrets/domain | grep "Not yet set.\n"
+then
+    echo "https://$DOCKER_HOSTNAME:6052" > /run/frames/esphomeyaml
+    echo "https://$DOCKER_HOSTNAME/docs" > /run/frames/docs
+    echo "https://$DOCKER_HOSTNAME/node_red" > /run/frames/node_red
+    echo "https://$DOCKER_HOSTNAME/portainer" > /run/frames/portainer
+else 
+    echo "https://$(cat /run/secrets/domain):6052" > /run/frames/esphomeyaml
+    echo "https://$(cat /run/secrets/domain)/docs" > /run/frames/docs
+    echo "https://$(cat /run/secrets/domain)/node_red" > /run/frames/node_red
+    echo "https://$(cat /run/secrets/domain)/portainer" > /run/frames/portainer
+fi
