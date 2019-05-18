898eca8ebf62c0d2a1192a14c59066ff95980458
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-05-17 20:34:51 -0600

    Clean up side link generation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,11 @@ do
 done
 
 mkdir /run/frames
-# TODO: I think this should always use /run/secrets/domain, and throw an error 
-#       if it's not yet set.
-if cat /run/secrets/domain | grep "Not yet set.\n"
-then
-    echo "https://$DOCKER_HOSTNAME:6052" > /run/frames/esphomeyaml
-    echo "https://$DOCKER_HOSTNAME/docs" > /run/frames/docs
-    echo "https://$DOCKER_HOSTNAME/node_red" > /run/frames/node_red
-    echo "https://$DOCKER_HOSTNAME/portainer" > /run/frames/portainer
-    echo "https://$DOCKER_HOSTNAME/grafana" > /run/frames/grafana
-    echo "https://$DOCKER_HOSTNAME/logs" > /run/frames/logs
-    echo "https://$DOCKER_HOSTNAME/jupyter" > /run/frames/jupyter
-    echo "https://$DOCKER_HOSTNAME/status" > /run/frames/status
-else 
-    echo "https://$(cat /run/secrets/domain):6052" > /run/frames/esphomeyaml
-    echo "https://$(cat /run/secrets/domain)/docs" > /run/frames/docs
-    echo "https://$(cat /run/secrets/domain)/node_red" > /run/frames/node_red
-    echo "https://$(cat /run/secrets/domain)/portainer" > /run/frames/portainer
-    echo "https://$(cat /run/secrets/domain)/grafana" > /run/frames/grafana
-    echo "https://$(cat /run/secrets/domain)/logs" > /run/frames/logs
-    echo "https://$(cat /run/secrets/domain)/jupyter" > /run/frames/jupyter
-    echo "https://$(cat /run/secrets/domain)/status" > /run/frames/status
-fi
+echo "https://$(cat /run/secrets/domain):6052" > /run/frames/esphomeyaml
+echo "https://$(cat /run/secrets/domain)/docs" > /run/frames/docs
+echo "https://$(cat /run/secrets/domain)/node_red" > /run/frames/node_red
+echo "https://$(cat /run/secrets/domain)/portainer" > /run/frames/portainer
+echo "https://$(cat /run/secrets/domain)/grafana" > /run/frames/grafana
+echo "https://$(cat /run/secrets/domain)/logs" > /run/frames/logs
+echo "https://$(cat /run/secrets/domain)/jupyter" > /run/frames/jupyter
+echo "https://$(cat /run/secrets/domain)/health" > /run/frames/health
