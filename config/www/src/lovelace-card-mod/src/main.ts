552c52ada671e110003611180c1ce0ef51acd3dd
Spencer Hachmeister <tms@spencerslab.com>
2021-04-25 19:04:18 -0600

    git subrepo clone https://github.com/thomasloven/lovelace-card-mod.git config/www/src/lovelace-card-mod
    subrepo:
      subdir:   "config/www/src/lovelace-card-mod"
      merged:   "c7e9342c4"
    upstream:
      origin:   "https://github.com/thomasloven/lovelace-card-mod.git"
      branch:   "master"
      commit:   "c7e9342c4"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

---
+++ config/www/src/lovelace-card-mod/src/main.ts
@@ -0,0 +1,16 @@
+import { fireEvent } from "card-tools/src/event.js";
+import "./card-mod";
+import "./patch/ha-card";
+import "./patch/hui-entities-card";
+import "./patch/hui-glance-card";
+import "./patch/hui-state-label-badge";
+import "./patch/hui-view";
+import "./patch/hui-root";
+import "./patch/ha-more-info-dialog";
+import "./patch/ha-sidebar";
+import "./patch/hui-card-element-editor";
+import "./patch/hui-picture-elements-card";
+import "./patch/ha-icon";
+import "./mod-card";
+import "./theme-watcher";
+fireEvent("ll-rebuild", {});
